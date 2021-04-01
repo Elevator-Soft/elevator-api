dd5305d4d1bad61ee40a5681ef6631492ef4b500
Шабалков Дмитрий Владимирович <shabalkov@kontur>
2021-04-01 22:09:39 +0500

    add new migration

---
+++ Repositories/Migrations/20210401170629_AddProject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repositories.Migrations
+{
+    public partial class AddProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Example");
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    ProjectUri = table.Column<string>(type: "text", nullable: true),
+                    GitToken = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.CreateTable(
+                name: "Example",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Example", x => x.Id);
+                });
+        }
+    }
+}
