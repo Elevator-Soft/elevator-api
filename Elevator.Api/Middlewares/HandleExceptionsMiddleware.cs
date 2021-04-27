d041b7bfc4b0f3f900f45808ff7776893dae1b9b
a.isakov <a.isakov@skbkontur.ru>
2021-04-27 14:01:26 +0500

    hotfix
    im colorblind i dont know

--- Elevator.Api/Middlewares/HandleExceptionsMiddleware.cs
+++ Elevator.Api/Middlewares/HandleExceptionsMiddleware.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Common;
 using Elevator.Api.Exceptions;
-using Elevator.Api.Utils;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 
